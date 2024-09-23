20ed4804fe4c0594d4b1109ef84e8b754b38e135
anthony-th <yefremov.anthony@gmail.com>
2024-09-24 02:38:09 +0300

    refactor: navbar links upd

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import Logo from '@/assets/Logo.webp';
 import Link from './Link';
@@ -19,30 +19,31 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   useEffect(() => {
-    if (isMenuToggled) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
-
+    document.body.style.overflow = isMenuToggled ? 'hidden' : 'auto';
     return () => {
       document.body.style.overflow = 'auto';
     };
   }, [isMenuToggled]);
 
+  const links = ["Home", "Benefits", "Our Classes", "Contact Us"];
+
   return (
     <nav>
-      <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-40 w-full py-6`}>
+      <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img alt="logo" src={Logo} width={113} height={24} loading="lazy" />
             {isAboveMediumScreens ? (
               <div className={`${flexBetween} w-full gap-4`}>
                 <div className={`${flexBetween} gap-8 text-sm`}>
-                  <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                  <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                  <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                  <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                  {links.map(link => (
+                    <Link 
+                      key={link}
+                      page={link} 
+                      selectedPage={selectedPage} 
+                      setSelectedPage={setSelectedPage} 
+                    />
+                  ))}
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p className='text-nowrap'>Sign In</p>
@@ -61,7 +62,7 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
         </div>
       </div>
       {!isAboveMediumScreens && isMenuToggled && (
-        <>
+        <Fragment>
           <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl flex flex-col justify-between md:block">
             <div className="flex justify-end p-6">
               <button onClick={() => setIsMenuToggled(false)}>
@@ -69,33 +70,21 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
               </button>
             </div>
             <div className="px-[20%] flex flex-col gap-10 h-full py-8">
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Benefits"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Our Classes"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Contact Us"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
+              {links.map(link => (
+                <Link 
+                  key={link}
+                  page={link} 
+                  selectedPage={selectedPage} 
+                  setSelectedPage={setSelectedPage} 
+                />
+              ))}
             </div>
           </div>
           <div
             className="fixed inset-0 z-30 bg-black bg-opacity-50"
             onClick={() => setIsMenuToggled(false)}
           ></div>
-        </>
+        </Fragment>
       )}
     </nav>
   );
